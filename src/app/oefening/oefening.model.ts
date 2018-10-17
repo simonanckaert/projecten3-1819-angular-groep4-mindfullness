bac40b33f7db4a7dd47d3d546b7ac524aafe3957
Arno Coorevits <arno.coorevits@sgsintpaulus.eu>
2018-10-17 11:59:12 +0200

    testcommit

--- src/app/oefening/oefening.model.ts
+++ src/app/oefening/oefening.model.ts
@@ -1,7 +1,9 @@
 export class Oefening {
+  private _id: number;
   private _naam: string;
   private _dateAdded: Date = new Date();
   private _beschrijving: string;
+// test;
  // private _oefening: Object;
 
   constructor(naam: string, beschrijving: string) {
@@ -9,6 +11,11 @@ export class Oefening {
     this._beschrijving = beschrijving;
   //  this._oefening = oefening;
   }
+
+  get id(): number {
+    return this._id;
+  }
+
   get naam(): string {
     return this._naam;
   }
