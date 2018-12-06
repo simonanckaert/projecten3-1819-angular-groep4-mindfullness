995636657b56aef8489be1fb3e6d059e7fe9dc2b
Stijn <stijn.deraevezenner@student.hogent.be>
2018-12-06 15:31:28 +0100

    Fix regio telnr

--- src/app/gebruikers/gebruikers.component.ts
+++ src/app/gebruikers/gebruikers.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs';
-import { MatTableDataSource } from '@angular/material';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { GebruikerDataService } from '../gebruiker-data.service';
-import { filter } from 'rxjs/operators';
+import { Component, OnInit, Inject } from "@angular/core";
+import { AngularFireDatabase } from "angularfire2/database";
+import { Observable } from "rxjs";
+import {
+  MatTableDataSource,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogConfig,
+  MatDialog
+} from "@angular/material";
+import { AngularFireAuth } from "angularfire2/auth";
+import { GebruikerDataService } from "../gebruiker-data.service";
+import * as admin from "firebase-admin";
 
 export interface Groep {
   value: string;
@@ -12,24 +18,28 @@ export interface Groep {
 }
 
 @Component({
-  selector: 'app-gebruikers',
-  templateUrl: './gebruikers.component.html',
-  styleUrls: ['./gebruikers.component.css']
+  selector: "app-gebruikers",
+  templateUrl: "./gebruikers.component.html",
+  styleUrls: ["./gebruikers.component.css"]
 })
 export class GebruikersComponent implements OnInit {
-
   private _gebruikers: Observable<any[]>;
 
   public groepen: Groep[] = [];
-  public groepNummers = [' A '];
-  public selectedGroepNr = ' A ';
+  public groepNummers = [" A "];
+  public selectedGroepNr = " A ";
 
-  public selectedGroep = 'alle gebruikers';
+  public selectedGroep = "alle gebruikers";
 
-  displayedColumns: string[] = ['name', 'email', 'group'];
+  displayedColumns: string[] = ["name", "email", "group", "delete"];
   dataSource: MatTableDataSource<any>;
 
-  constructor(public afDb: AngularFireDatabase, public af: AngularFireAuth, public gService: GebruikerDataService) {
+  constructor(
+    public afDb: AngularFireDatabase,
+    public af: AngularFireAuth,
+    public gService: GebruikerDataService,
+    public dialog: MatDialog
+  ) {
     this._gebruikers = this.getItems();
     this._gebruikers.subscribe(result => {
       this.setGroepen(result);
@@ -46,11 +56,14 @@ export class GebruikersComponent implements OnInit {
     return this._gebruikers;
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   setGroepen(result: any[]) {
     result.forEach(gebruiker => {
-      if (gebruiker.groepnr !== undefined && this.groepNummers.indexOf(gebruiker.groepnr) === -1) {
+      if (
+        gebruiker.groepnr !== undefined &&
+        this.groepNummers.indexOf(gebruiker.groepnr) === -1
+      ) {
         this.groepNummers.push(gebruiker.groepnr);
       }
     });
@@ -58,10 +71,10 @@ export class GebruikersComponent implements OnInit {
     this.groepNummers.sort();
 
     this.groepNummers.forEach(nummer => {
-      if (nummer !== ' A ') {
-        this.groepen.push({ value: nummer, viewValue: 'Groep ' + nummer });
+      if (nummer !== " A ") {
+        this.groepen.push({ value: nummer, viewValue: "Groep " + nummer });
       } else {
-        this.groepen.push({ value: nummer, viewValue: 'Geen' });
+        this.groepen.push({ value: nummer, viewValue: "Geen groep" });
       }
     });
   }
@@ -73,7 +86,7 @@ export class GebruikersComponent implements OnInit {
   applyGroepFilter(filterValue: string) {
     this._gebruikers.subscribe(result => {
       this.dataSource = new MatTableDataSource(result);
-      if (filterValue !== ' A ') {
+      if (filterValue !== " A ") {
         const newData = [];
         this.dataSource.data.forEach(element => {
           if (element.groepnr === filterValue) {
@@ -81,11 +94,11 @@ export class GebruikersComponent implements OnInit {
           }
         });
         this.dataSource = new MatTableDataSource(newData);
-        this.selectedGroep = 'Groep ' + filterValue;
-        this.selectedGroepNr = filterValue
+        this.selectedGroep = "Groep " + filterValue;
+        this.selectedGroepNr = filterValue;
       } else {
-        this.selectedGroep = 'alle gebruikers';
-        this.selectedGroepNr = ' A '
+        this.selectedGroep = "alle gebruikers";
+        this.selectedGroepNr = " A ";
       }
     });
   }
@@ -93,19 +106,76 @@ export class GebruikersComponent implements OnInit {
   changeGroup(uid, nr) {
     const gebruiker = this.gService.getUserById(uid);
     gebruiker.subscribe(result => {
+      let telnr = 0;
+      if (result.telnr) {
+        telnr = result.telnr;
+      }
+
+      let regio = "";
+      if (result.regio) {
+        regio = result.regio;
+      }
+
       const updatedGebruiker = {
-        'email': result.email,
-        'groepnr': nr.value.toString(),
-        'name': result.name
+        email: result.email,
+        groepnr: nr.value.toString(),
+        name: result.name,
+        tel: telnr,
+        regio: regio
       };
-      this.gService.updateUser(uid, updatedGebruiker).subscribe((val) => console.log(val));
+      this.gService
+        .updateUser(uid, updatedGebruiker)
+        .subscribe(val => console.log(val));
+      //this.openSnackBar(updatedGebruiker.name, "ok");
     });
   }
 
+  /*openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }*/
+
   addGroup() {
-    const n: number = +this.groepen[this.groepen.length-1].value + 1
-    console.log(n)
-    this.groepen.push({ value: n.toString(), viewValue: 'Groep ' + n });
+    const n: number = +this.groepen[this.groepen.length - 1].value + 1;
+    console.log(n);
+    this.groepen.push({ value: n.toString(), viewValue: "Groep " + n });
     this.groepNummers.push(n.toString());
+  }
+
+  // Show alert dialog before removing user
+  removeUser(uid): void {
+    const gebruiker = this.gService.getUserById(uid);
+    gebruiker.subscribe(result => {
+      const dialogRef = this.dialog.open(DialogAlert, {
+        minWidth: 300,
+        data: {
+          dataName: result.name,
+          dataUid: uid
+        }
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          // REMOVE USER
+          console.log("Not yet implemented");
+        }
+      });
+    });
+  }
+}
+
+@Component({
+  selector: "app-dialog-alert",
+  templateUrl: "./dialog.alert.html"
+})
+export class DialogAlert {
+  constructor(
+    public dialogRef: MatDialogRef<DialogAlert>,
+    @Inject(MAT_DIALOG_DATA) public data
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
