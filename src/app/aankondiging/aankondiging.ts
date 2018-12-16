9510bc602cfbd93ce14faddb80a8efaf1d16549a
Stijn De Raeve-Zenner <stijn.deraevezenner@student.hogent.be>
2018-12-16 18:27:31 +0100

    Fixed some code + updated aankondigingen mat-card

--- src/app/aankondiging/aankondiging.ts
+++ src/app/aankondiging/aankondiging.ts
@@ -1,83 +1,79 @@
 export class Announcement {
-
-	
     public _key: string;
     public _text: string;
     public _date: Date;
     public _group: string;
 
-    
-
-	constructor(key: string, text: string, date: Date, group: string) {
-		this._key = key;
-		this._text = text;
-		this._date = date;
-		this._group = group;
-	}
+    constructor(key: string, text: string, date: Date, group: string) {
+        this._key = key;
+        this._text = text;
+        this._date = date;
+        this._group = group;
+    }
 
 
     /**
      * Getter key
      * @return {string}
      */
-	public get key(): string {
-		return this._key;
-	}
+    public get key(): string {
+        return this._key;
+    }
 
     /**
      * Getter text
      * @return {string}
      */
-	public get text(): string {
-		return this._text;
-	}
+    public get text(): string {
+        return this._text;
+    }
 
     /**
      * Getter date
      * @return {Date}
      */
-	public get date(): Date {
-		return this._date;
-	}
+    public get date(): Date {
+        return this._date;
+    }
 
     /**
      * Getter group
      * @return {string}
      */
-	public get group(): string {
-		return this._group;
-	}
+    public get group(): string {
+        return this._group;
+    }
 
     /**
      * Setter key
      * @param {string} value
      */
-	public set key(value: string) {
-		this._key = value;
-	}
+    public set key(value: string) {
+        this._key = value;
+    }
 
     /**
      * Setter text
      * @param {string} value
      */
-	public set text(value: string) {
-		this._text = value;
-	}
+    public set text(value: string) {
+        this._text = value;
+    }
 
     /**
      * Setter date
      * @param {Date} value
      */
-	public set date(value: Date) {
-		this._date = value;
-	}
+    public set date(value: Date) {
+        this._date = value;
+    }
 
     /**
      * Setter group
      * @param {string} value
      */
-	public set group(value: string) {
-		this._group = value;
-	}
+    public set group(value: string) {
+        this._group = value;
+    }
 
 }
